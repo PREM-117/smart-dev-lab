d897389be5041eec8b7a57d615498acb9a85f747
Prem <119773607+PREM-117@users.noreply.github.com>
2026-04-12 16:00:17 +0530

    Add script to stop VNC server and exit Debian

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+echo "[+] Stopping VNC..."
+proot-distro login debian -- bash -c "vncserver -kill :1"
+
+echo "[+] Exiting Debian..."
+proot-distro login debian -- bash -c "exit"
+
+echo "[+] Stopped"
